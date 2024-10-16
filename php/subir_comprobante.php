f6610921661ada8798fe6ada966614c58bdf1055
Rene <renepersonal931@gmail.com>
2024-10-16 10:54:49 -0500

    proyeccion67

--- php/subir_comprobante.php
+++ php/subir_comprobante.php
@@ -28,12 +28,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Manejar la subida de la imagen (comprobante de envío)
     $comprobanteEnvioRuta = '';
     if (isset($_FILES['comprobanteEnvio']) && $_FILES['comprobanteEnvio']['error'] == UPLOAD_ERR_OK) {
-        $target_dir = "php/uploads/";  // Directorio donde se guardará el archivo
+        $target_dir = "uploads/";  // Directorio donde se guardará el archivo
         $target_file = $target_dir . basename($_FILES["comprobanteEnvio"]["name"]);
         
         // Mover el archivo subido al directorio deseado
         if (move_uploaded_file($_FILES["comprobanteEnvio"]["tmp_name"], $target_file)) {
-            $comprobanteEnvioRuta = "uploads/" . basename($_FILES["comprobanteEnvio"]["name"]);  // Guardar la ruta correcta en la base de datos
+            $comprobanteEnvioRuta = basename($_FILES["comprobanteEnvio"]["name"]);  // Guardar solo el nombre del archivo
         } else {
             echo "Error al subir el archivo.";
             exit();
